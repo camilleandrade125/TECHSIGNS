a5fb879ca02df63a6899f800f6b121092206c536
Lianderdev <liandervinicius13@gmail.com>
2024-02-03 18:15:37 -0300

    page-niveis-atualiazando

--- src/components/niveis/index.jsx
+++ src/components/niveis/index.jsx
@@ -1,10 +1,4 @@
 import "./style.css"
-import img_nivel_1 from '../../../public/img-card-nivel1.png'
-import img_nivel_2 from '../../../public/img-card-nivel2.png'
-import img_nivel_3 from '../../../public/img-card-nivel3.png'
-import img_nivel_4 from '../../../public/img-card-nivel4.png'
-import img_nivel_5 from '../../../public/img-card-nivel5.png'
-import img_fundo_niveis from '../../../public/techsings-fundo-niveis.jpg'
 import { Link } from "react-router-dom"
 
 
@@ -13,50 +7,27 @@ function Niveis(){
      
         <>
          <main>
-          
-          <section className="section-img">
-          </section>
-
-
-
-          <section className="section-card-nivel">
-              <div className="flex-nivel">
-                  <div className="card-nivel">
-                      <img src={img_nivel_1}/>
-                      <div>
-                          <Link to="/g1nivel1" className="link-jogar">JOGAR</Link>
-                      </div>
-                  </div>
-
-                  <div className="card-nivel">
-                      <img src={img_nivel_2}/>
-                      <div>
-                        <Link to="/g1nivel2" className="link-jogar">JOGAR</Link>
-                      </div>
-                  </div>
-
-                  <div className="card-nivel">
-                      <img src={img_nivel_3}/>  
-                      <div>
-                        <Link to="/g1nivel3"  className="link-jogar">JOGAR</Link>
-                      </div>
-                  </div>
-
-                  <div className="card-nivel">
-                      <img src={img_nivel_4}/>  
-                      <div>
-                        <Link to="/g1nivel4" className="link-jogar">JOGAR</Link>
-                      </div>
-                  </div>
-
-                  <div className="card-nivel">
-                      <img src={img_nivel_5}/>  
-                      <div>
-                        <Link to="/g1nivel5" className="link-jogar">JOGAR</Link>
-                      </div>
-                  </div>
+          <section className="card-flex">
+            <div className="cards">
+            <div className="niveis">
+            <h3>NÍVEL</h3>
+            <span className="nivel">1</span>
+            </div>
+            <div className="card-metade">
+              <span className="fases">Fases</span>
+                <div className="botoes-flex">
+                  <Link className="botoes-jogar-1 botoes-style"></Link>
+                  <Link className="botoes-jogar-2 botoes-style"></Link>
+                  <Link className="botoes-jogar-3 botoes-style"></Link>
+                  <Link className="botoes-jogar-4 botoes-style"></Link>
+                  <Link className="botoes-jogar-5 botoes-style"></Link>
                 </div>
-           </section>
+              </div>
+            </div>
+
+            
+
+          </section>
          </main>
         </>
     ) 
