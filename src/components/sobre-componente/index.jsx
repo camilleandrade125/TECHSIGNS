555f785c5c6d49828773790163a248cbed9cf1d8
camilleandrade125 <camilleandrade125@gmail.com>
2024-02-05 12:45:35 -0300

    Atualizando tela de sobre

--- src/components/sobre-componente/index.jsx
+++ src/components/sobre-componente/index.jsx
@@ -4,8 +4,10 @@ import CardInformacoes from "../cards/card-informacoes";
 function Bodysobre(){
     return  (
         <section className="space-sobre">
-            <main className="infos-card-space"> 
+            <main className="infos-card-space">
 
+
+       
                 <CardInformacoes
                     fotoPerfil="https://avatars.githubusercontent.com/u/98554053?v=4"
                     cargo="Front-End Developer" 
@@ -45,18 +47,7 @@ function Bodysobre(){
                     numeroTelefone="981178669" 
                     nome="Eduador Victor"
                 />
-            </main>
-            <section className="space-sobre-menor">
-
-                    <h2>Sobre o projeto</h2>
-
-                    <p>O TechSigns é um software contendo uma variedade de jogos educativos adptados à Libras (Língua Brasileira de Sinais), com o propósito de fomentar a eduacção inclusiva. O software busca proporcionar acessibildade e inclusão no processo de ensino-aprendizagem de estudantes surdos. Os jogos buscam oferecer uma abordagem gamifiada para compreensão de conceitos fundamentais de informática.</p>
-
-                    <div className="divt"></div>
-
-            </section>
-            <div className="div"></div>
-            
+            </main>         
             
         </section>
 
--- src/pages/sobre/index.jsx
+++ src/pages/sobre/index.jsx
@@ -6,6 +6,7 @@ function MenuSobre(){
     return(
         <>
         <Bodysobre/>
+
         </>
     )
 }
