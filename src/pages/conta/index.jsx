d548a02aca1b02324a3e1d2358203c473caf05f7
Lianderdev <liandervinicius13@gmail.com>
2024-02-21 16:45:41 -0300

    page perfil

--- src/pages/conta/index.jsx
+++ src/pages/conta/index.jsx
@@ -1,21 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import "./style.css"
-import { useState } from 'react'
-import robo from '../../../public/robo!.gif'
-import  Botao_Voltar  from "../../components/botao-voltar"
-import maos_feed from '../../../public/feed-mãos.jpg'
+import { useLocation } from 'react-router-dom';
 
-function Feedbackpage(){
+const Profile = () => {
+  const [userData, setUserData] = useState({});
+  const location = useLocation();
+  const token = location.state?.token;
 
-    return(
-        <>
-        <Botao_Voltar link_voltar='/menu'/>
-        <main className="perfil">
-            <section className="area-info">
-                   <h1 className="dese">EM DESENVOLVIMENTO!</h1>
-            </section>
-        </main>
-        </>
-    )
-}
+  useEffect(() => {
+    const getProfileData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/profile', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-export default Feedbackpage
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Erro ao obter dados do perfil:', error.message);
+      }
+    };
+
+    getProfileData();
+  }, [token]);
+
+  return (
+    <div className='perfil'>
+      <h2>Perfil do Usuário</h2>
+      <ul>
+        <li>Nome: {userData.nome_completo}</li>
+        <li>Email: {userData.email}</li>
+        <li>CPF: {userData.cpf}</li>
+        <li>Gênero: {userData.genero}</li>
+        <li>Data de Nascimento: {userData.data_nascimento}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
